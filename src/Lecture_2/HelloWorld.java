3e81ee62ec40b511907c584b7ad8a1210289fa54
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-01 10:30:52 +0530

    Initial commit

--- src/Lecture_2/HelloWorld.java
+++ src/Lecture_2/HelloWorld.java
@@ -3,6 +3,8 @@ package Lecture_2;
 public class HelloWorld {
 	
 	public static void main (String args []) {
+		
+		//if you use println will put a newline at the end.
 		System.out.println("Hello World");
 		System.out.print("Hello World");
 		System.out.println("Hello World");
