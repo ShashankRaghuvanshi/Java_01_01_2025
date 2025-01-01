5cc6b338e27a1c441810a7dcd78759e694602d76
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-01 09:50:48 +0530

    Initial commit for project setup

---
+++ src/Lecture_2/HelloWorld.java
@@ -0,0 +1,11 @@
+package Lecture_2;
+
+public class HelloWorld {
+	
+	public static void main (String args []) {
+		System.out.println("Hello World");
+		System.out.print("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
