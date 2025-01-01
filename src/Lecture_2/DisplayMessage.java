9767f616a3b44d00d16b55b3746aa80542437961
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-01 10:58:37 +0530

    update

---
+++ src/Lecture_2/DisplayMessage.java
@@ -0,0 +1,13 @@
+package Lecture_2;
+
+public class DisplayMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welocome to Java Course!");
+		System.out.println("Hello");
+		System.out.println("Ninja");
+
+	}
+
+}
