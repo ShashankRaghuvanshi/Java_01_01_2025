7de4ae063d16d7398d0989a3d9cdea467ade459b
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-01 12:39:36 +0530

    update

---
+++ src/Lecture_2/Operators.java
@@ -0,0 +1,28 @@
+package Lecture_2;
+
+import java.util.Scanner;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		//int f = sc.nextInt();
+		
+//		int c = 5*(f-32)/9;
+		
+//		int c = (int)((5.0/9)*(f-32));
+//		
+//		System.out.println(c);
+		
+		System.out.println('A'+1); //66
+		
+		char c = 'a';
+		char i = (char)(c + 3);
+		System.out.println(i);  //d
+		
+		System.out.println('a'+'b');  //195
+	}
+
+}
