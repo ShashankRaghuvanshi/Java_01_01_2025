7392c705e31d4b0311fef3820ffe054986b8e43a
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-01 11:26:06 +0530

    update

---
+++ src/Lecture_2/SI_Calculator.java
@@ -0,0 +1,15 @@
+package Lecture_2;
+
+public class SI_Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p = 1000;
+		int r = 5;
+		int t =3;
+		int si= (p*r*t)/100;
+		
+		System.out.println(si);
+	}
+
+}
---
+++ src/Lecture_2/Understanding_Variables.java
@@ -0,0 +1,31 @@
+package Lecture_2;
+
+public class Understanding_Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//add 2 numbers
+		int i = 10;
+		int j = 15;
+		
+		System.out.println(i+j);
+		
+		short k = 5;
+		
+		long l = 23;
+		
+		double d = 3.23;
+		
+		float f = 12.00f;
+		
+		//float r;
+		//System.out.println(r);
+		
+		char ch = 'a';
+		
+		boolean b = true;
+
+	}
+
+}
