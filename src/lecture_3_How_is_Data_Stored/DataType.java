ce70decabd9355c9c5850a78c928606d65f64bf1
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-02 06:29:05 +0530

    Commit

---
+++ src/lecture_3_How_is_Data_Stored/DataType.java
@@ -0,0 +1,24 @@
+package lecture_3_How_is_Data_Stored;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'a';
+		System.out.println(ch + 3);
+		
+		int i = ch + 3;
+		System.out.println(i);
+		
+		char abc = (char) i;
+		System.out.println(abc);
+		
+		double d = i;
+		System.out.println(d);
+		
+		float f = 4.3f;
+		System.out.println(f);
+				
+	}
+
+}
