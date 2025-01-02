b7266acd181f9af84c6c62cfc0143872e4835fb8
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-02 06:34:47 +0530

    Commit

---
+++ src/lecture_3_How_is_Data_Stored/TypeCasting.java
@@ -0,0 +1,21 @@
+package lecture_3_How_is_Data_Stored;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 100;
+		
+		long l = i;
+		
+		i = (int)l;
+		
+		l = 123;
+		
+		System.out.println(l);
+		
+		l = 234234234423l;
+		System.out.println(l);
+	}
+
+}
