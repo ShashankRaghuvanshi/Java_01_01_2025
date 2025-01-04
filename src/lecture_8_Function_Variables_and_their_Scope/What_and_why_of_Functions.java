0d5b1e6cc2376bd2ac8c789d2b0e6cd5b5cbbd94
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-04 11:28:55 +0530

    Commit

---
+++ src/lecture_8_Function_Variables_and_their_Scope/What_and_why_of_Functions.java
@@ -0,0 +1,36 @@
+package lecture_8_Function_Variables_and_their_Scope;
+
+import java.util.Scanner;
+
+public class What_and_why_of_Functions {
+	
+	public static int factorial(int n){
+		int ans = 1;
+		for(int i =1; i<=n; i++){
+			ans = ans * i;
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int r = sc.nextInt();
+		
+		int num = factorial(n);
+		
+		int den1 = factorial(r);
+		
+		int den2 = factorial(n-r);
+		
+		int ans = num/(den1 * den2);
+		
+		System.out.println(ans);
+		
+		sc.close();
+
+	}
+
+}
