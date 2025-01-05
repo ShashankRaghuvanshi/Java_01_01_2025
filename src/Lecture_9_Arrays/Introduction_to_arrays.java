5e3bc661c7b65d7b7ebe945e624186978a310e69
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-05 09:08:13 +0530

    Commit

---
+++ src/Lecture_9_Arrays/Introduction_to_arrays.java
@@ -0,0 +1,30 @@
+package Lecture_9_Arrays;
+
+import java.util.Scanner;
+
+public class Introduction_to_arrays {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int [] arr = new int[10];
+		arr[3] = 15;
+		System.out.println(arr[3]);
+		System.out.println(arr[0]);
+		System.out.println(arr[5]);
+		System.out.println(arr);
+		
+		char charr[] = new char[5];
+		
+		double d[] = new double[5];
+		
+		System.out.println(charr[0]);
+		
+		System.out.println(d[0]);
+		
+		
+
+	}
+
+}
