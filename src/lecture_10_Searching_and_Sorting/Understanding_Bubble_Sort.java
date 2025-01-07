cad5ab5e35a303581bdf25196280b25954c263b7
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-07 06:52:58 +0530

    Commit

--- src/lecture_10_Searching_and_Sorting/Understanding_Bubble_Sort.java
+++ src/lecture_10_Searching_and_Sorting/Understanding_Bubble_Sort.java
@@ -1,7 +1,36 @@
 package lecture_10_Searching_and_Sorting;
+import java.util.Scanner;
 
 public class Understanding_Bubble_Sort {
 	
+	public static void bubbleSort(int[] arr, int n) {
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+	}
 	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i] = sc.nextInt();
+		}
+		bubbleSort(arr, n);
+        for(int i=0;i<n;i++)
+       	{
+       		System.out.print(arr[i]+" ");
+        }
+		
+	}
 
 }
