1423efc6d87d0cdf010b4f7f136e72318eab5448
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-09 06:41:41 +0530

    Commit

---
+++ src/lecture_11_Strings/Intoduction_to_Strings.java
@@ -0,0 +1,17 @@
+package lecture_11_Strings;
+
+public class Intoduction_to_Strings {
+
+	public static void main(String[] args) {
+		
+		char arr[] = {'c','o','d','i','n','g'};
+		String str = "Coding";
+		String str1 = "";
+		String str2 = " ";
+		System.out.println(str.length());
+		System.out.println(str.charAt(2));
+		System.out.println(str1.length());
+		System.out.println(str2.length());
+	}
+
+}
